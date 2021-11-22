78103f526c1a9e73cc01b6b8fa7cc5e8e82ea110
Maxime Bus <bus.maxime@mailoo.org>
2021-11-22 13:28:52 +0100

    Add code snippet to make scalingo happy
    I had an issue on scalingo while trying to deploy the bot. After 60s,
    the application was shut down by scalingo as the app wasn't binding on
    port 80 during that time.
    
    https://doc.scalingo.com/platform/getting-started/common-deployment-errors#start-errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {DiscordBotConfiguration, registerCommands} from "./discord-bot/bot-command-registration";
 import {LgptBot} from "./discord-bot/discord-bot";
 import {createAxiosInstance, LolRestClient} from "./lol-client/lol-rest-client";
+import * as http from "http";
 
 const exitWithFailureIfNotProvided = (requiredValue: any, errorMessage: string) => {
     if (!requiredValue) {
@@ -29,6 +30,13 @@ function main() {
         .login(
             // @ts-ignore
             process.env.LGPT_BOT_TOKEN);
+
+    // This part is only useful for scalingo.
+    // It requires that our app listen on 80 port ($PORT).
+    if (process.env.NODE_ENV != 'dev') {
+        const server = http.createServer();
+        server.listen(process.env.PORT);
+    }
 }
 
 main();
